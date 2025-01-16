b099e5ef8c28115e47e5cc81c1d8593536403c42
jaocodes <jhonskaz@gmail.com>
2025-01-15 21:58:02 -0300

    refatora as entidades Answer e Question

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,11 +1,15 @@
 import type { Slug } from './value-objets/slug'
 import { Entity } from '../../core/entities/entity'
+import type { UniqueEntityID } from '../../core/entities/unique-entity-id'
 
 interface QuestionProps {
   title: string
   slug: Slug
   content: string
-  authorId: string
+  bestAnswerId?: UniqueEntityID
+  authorId: UniqueEntityID
+  createdAt: Date
+  updatedAt?: Date
 }
 
 export class Question extends Entity<QuestionProps> {}
