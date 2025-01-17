4686707b0a0ae76ba1e7d5abb1ee29d034eb8130
jaocodes <jhonskaz@gmail.com>
2025-01-17 17:48:19 -0300

    adiciona Faker.js para gerar dados fictícios e atualiza a fábrica de perguntas

--- test/factories/make-questions.ts
+++ test/factories/make-questions.ts
@@ -1,16 +1,24 @@
+import { faker } from '@faker-js/faker'
+
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import {
   Question,
   type QuestionProps,
 } from '@/domain/forum/enterprise/entities/question'
 
-export function makeQuestion(override: Partial<QuestionProps> = {}) {
-  const question = Question.create({
-    authorId: new UniqueEntityID('1'),
-    title: 'Example question',
-    content: 'Example question content',
-    ...override,
-  })
+export function makeQuestion(
+  override: Partial<QuestionProps> = {},
+  id?: UniqueEntityID,
+) {
+  const question = Question.create(
+    {
+      authorId: new UniqueEntityID('1'),
+      title: faker.lorem.sentence(),
+      content: faker.lorem.text(),
+      ...override,
+    },
+    id,
+  )
 
   return question
 }
