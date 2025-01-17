4191cc44794909d8d6c832d1172fbb2538a1f405
jaocodes <jhonskaz@gmail.com>
2025-01-17 13:29:24 -0300

    remove interface AnswerRepository

--- src/domain/forum/application/repositories/answer-repository.ts
+++
@@ -1,7 +0,0 @@
-import type { Answer } from '../../enterprise/entities/answer'
-
-export interface AnswerRepository {
-  create(answer: Answer): Promise<void>
-}
-
-// define apenas contratos para camada de persistência
