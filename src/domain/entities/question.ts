210968391033a2317c7c746ab4ff59448571bbef
jaocodes <jhonskaz@gmail.com>
2025-01-16 22:00:38 -0300

    adiciona getters and setters a entidades

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,11 +1,11 @@
-import type { Slug } from './value-objets/slug'
+import { Slug } from './value-objets/slug'
 import { Entity } from '../../core/entities/entity'
 import type { UniqueEntityID } from '../../core/entities/unique-entity-id'
 import type { Optional } from '../../core/@types/optional'
 
 interface QuestionProps {
   title: string
-  slug: Slug
+  slug?: Slug
   content: string
   bestAnswerId?: UniqueEntityID
   authorId: UniqueEntityID
@@ -21,11 +21,58 @@ export class Question extends Entity<QuestionProps> {
     const question = new Question(
       {
         ...props,
+        slug: props.slug ?? Slug.createFromText(props.title),
         createdAt: new Date(),
       },
       id,
     )
 
     return question
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date()
+  }
+
+  get authorId() {
+    return this.props.authorId
+  }
+  get title() {
+    return this.props.title
+  }
+  get slug() {
+    return this.props.slug
+  }
+  get content() {
+    return this.props.content
+  }
+  get createdAt() {
+    return this.props.createdAt
+  }
+  get updatedAt() {
+    return this.props.updatedAt
+  }
+  get bestAnswerId() {
+    return this.props.bestAnswerId
+  }
+
+  get excerpt() {
+    return this.content.substring(0, 120).trimEnd().concat('...')
+  }
+
+  set title(title: string) {
+    this.props.title = title
+    this.props.slug = Slug.createFromText(title)
+    this.touch()
+  }
+
+  set content(content: string) {
+    this.props.content = content
+    this.touch()
+  }
+
+  set bestAnswerId(bestAnswerId: UniqueEntityID | undefined) {
+    this.props.bestAnswerId = bestAnswerId
+    this.touch()
   }
 }
