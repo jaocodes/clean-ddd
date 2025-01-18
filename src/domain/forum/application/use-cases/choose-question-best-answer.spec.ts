eecf153d60f1aa9c627d430a8285500bd01fe0a5
jaocodes <jhonskaz@gmail.com>
2025-01-18 18:12:01 -0300

    adiciona caso de uso e testes para escolher a melhor resposta de uma pergunta, incluindo validações de autor

---
+++ src/domain/forum/application/use-cases/choose-question-best-answer.spec.ts
@@ -0,0 +1,72 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { makeQuestion } from 'test/factories/make-question'
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
+import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository'
+import { ChooseQuestionBestAnswerUseCase } from './choose-question-best-answer'
+import { makeAnswer } from 'test/factories/make-answer'
+
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let inMemoryAnswersRepository: InMemoryAnswersRepository
+let sut: ChooseQuestionBestAnswerUseCase
+
+describe('Choose question best answer use-case', () => {
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+    inMemoryAnswersRepository = new InMemoryAnswersRepository()
+
+    sut = new ChooseQuestionBestAnswerUseCase(
+      inMemoryQuestionsRepository,
+      inMemoryAnswersRepository,
+    )
+  })
+
+  it('should be able choose a question best answer', async () => {
+    const newQuestion = makeQuestion(
+      { authorId: new UniqueEntityID('author-01') },
+      new UniqueEntityID('question-01'),
+    )
+
+    await inMemoryQuestionsRepository.create(newQuestion)
+
+    const newAnswer = makeAnswer({
+      authorId: new UniqueEntityID('author-02'),
+      content: 'Resposta do author 02',
+      questionId: new UniqueEntityID('question-01'),
+    })
+
+    await inMemoryAnswersRepository.create(newAnswer)
+
+    await sut.execute({
+      authorId: 'author-01',
+      answerId: newAnswer.id.toString(),
+    })
+
+    expect(inMemoryQuestionsRepository.items[0].bestAnswerId).toEqual(
+      newAnswer.id,
+    )
+  })
+
+  it('should not be able to choose another user question best answer', async () => {
+    const newQuestion = makeQuestion(
+      { authorId: new UniqueEntityID('author-01') },
+      new UniqueEntityID('question-01'),
+    )
+
+    await inMemoryQuestionsRepository.create(newQuestion)
+
+    const newAnswer = makeAnswer({
+      authorId: new UniqueEntityID('author-02'),
+      content: 'Resposta do author 02',
+      questionId: new UniqueEntityID('question-01'),
+    })
+
+    await inMemoryAnswersRepository.create(newAnswer)
+
+    await expect(() => {
+      return sut.execute({
+        answerId: newAnswer.id.toString(),
+        authorId: 'author-02',
+      })
+    }).rejects.toBeInstanceOf(Error)
+  })
+})
