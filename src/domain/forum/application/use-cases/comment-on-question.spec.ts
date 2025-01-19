b266bbee4c6b4999d61508a5459611ed504aa45d
jaocodes <jhonskaz@gmail.com>
2025-01-19 10:29:13 -0300

    adiciona testes para o caso de uso de comentar em perguntas

---
+++ src/domain/forum/application/use-cases/comment-on-question.spec.ts
@@ -0,0 +1,37 @@
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
+import { InMemoryQuestionCommentsRepository } from 'test/repositories/in-memory-question-comments-repository'
+import { CommentOnQuestionUseCase } from './comment-on-question'
+import { makeQuestion } from 'test/factories/make-question'
+
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let inMemoryQuestionCommentsRepository: InMemoryQuestionCommentsRepository
+let sut: CommentOnQuestionUseCase
+
+describe('Comment on question use-case', () => {
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+    inMemoryQuestionCommentsRepository =
+      new InMemoryQuestionCommentsRepository()
+
+    sut = new CommentOnQuestionUseCase(
+      inMemoryQuestionsRepository,
+      inMemoryQuestionCommentsRepository,
+    )
+  })
+
+  it('should be able to comment on question', async () => {
+    const question = makeQuestion()
+    await inMemoryQuestionsRepository.create(question)
+
+    const { questionComment } = await sut.execute({
+      authorId: 'comment-author',
+      content: 'comment content',
+      questionId: question.id.toString(),
+    })
+
+    expect(questionComment.id).toBeTruthy()
+    expect(inMemoryQuestionCommentsRepository.items[0].id).toEqual(
+      questionComment.id,
+    )
+  })
+})
