4ffc02b1d4a642f2bcb8f1d739db124cfcac9d6e
jaocodes <jhonskaz@gmail.com>
2025-01-19 16:17:39 -0300

    adiciona classe AggregateRoot e refatora Entity para ser abstrata

--- src/domain/forum/enterprise/entities/question.ts
+++ src/domain/forum/enterprise/entities/question.ts
@@ -1,7 +1,7 @@
 import type { Optional } from '@/core/@types/optional'
-import { Entity } from '@/core/entities/entity'
 import type { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { Slug } from './value-objets/slug'
+import { AggregateRoot } from '@/core/entities/aggregate-root'
 
 export interface QuestionProps {
   title: string
@@ -13,7 +13,7 @@ export interface QuestionProps {
   updatedAt?: Date
 }
 
-export class Question extends Entity<QuestionProps> {
+export class Question extends AggregateRoot<QuestionProps> {
   static create(
     props: Optional<QuestionProps, 'createdAt' | 'slug'>,
     id?: UniqueEntityID,
