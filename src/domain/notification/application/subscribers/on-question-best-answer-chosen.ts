e850fe6ae187839c1981ebfb11ba06c441b53c91
jaocodes <jhonskaz@gmail.com>
2025-01-23 09:40:22 -0300

    registra subscriber para criação de notificações para o evento de escolha de melhor resposta

---
+++ src/domain/notification/application/subscribers/on-question-best-answer-chosen.ts
@@ -0,0 +1,39 @@
+import { DomainEvents } from '@/core/events/domain-events'
+import type { EventHandler } from '@/core/events/event-handler'
+import type { SendNotificationUseCase } from '../use-cases/send-notification'
+import type { AnswersRepository } from '@/domain/forum/application/repositories/answers-repository'
+import { QuestionBestAnswerChosenEvent } from '@/domain/forum/enterprise/events/question-best-answer-chosen-event'
+
+export class OnQuestionBestChosen implements EventHandler {
+  constructor(
+    private answersRepository: AnswersRepository,
+    private sendNotification: SendNotificationUseCase,
+  ) {
+    this.setupSubscriptions()
+  }
+
+  setupSubscriptions(): void {
+    DomainEvents.register(
+      this.sendQuestionBestAnswerNotification.bind(this),
+      QuestionBestAnswerChosenEvent.name,
+    )
+
+    // registra o subscriber
+  }
+
+  private async sendQuestionBestAnswerNotification({
+    question,
+    bestAnswerId,
+  }: QuestionBestAnswerChosenEvent) {
+    const answer = await this.answersRepository.findById(
+      bestAnswerId.toString(),
+    )
+    if (answer) {
+      await this.sendNotification.execute({
+        recipientId: answer.authorId.toString(),
+        title: 'Sua resposta foi escolhida como a melhor resposta',
+        content: `A resposta que você enviou em "${question.title.substring(0, 20).concat('...')}"`,
+      })
+    }
+  }
+}
